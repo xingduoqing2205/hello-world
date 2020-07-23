8fc2c02dc6c419c5a66ccc09e3f42731886b5d3a
Duoduo1312 <32792814+Duoduo1312@users.noreply.github.com>
2020-07-23 23:44:05 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWord.java
@@ -0,0 +1,12 @@
+package com.youmeek.demo;
+
+import java.awt.*;
+
+/*
+created by Youmeek on 2020
+ */
+public class HelloWord{
+    public static void main(String[] args) {
+        System.out.print("Hello Word");
+    }
+}
